930b525e968fe305b8c1d630247129626bb661f0
HariniKaira <151957916+HariniKaira@users.noreply.github.com>
2026-06-18 13:44:51 +0000

    Remove Django backend, switching to Express for course

--- octofit-tracker/backend/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import UsersView, ActivitiesView
-
-urlpatterns = [
-    path('users/', UsersView.as_view()),
-    path('activities/', ActivitiesView.as_view()),
-]
